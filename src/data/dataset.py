f43ea900b71cae9a9ee7c1f551299f43d1dd3f8a
Felix <felix-lang@live.at>
2025-06-08 11:58:09 +0200

    Change to CuPy

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,5 +1,5 @@
 from typing import List, Tuple
-import numpy as np
+import cupy as cp
 
 class CharDataset:
     def __init__(self, token_ids: List[int], seq_len: int, batch_size: int):
@@ -7,14 +7,14 @@ class CharDataset:
         self.seq_len = seq_len
         self.batch_size = batch_size
 
-    def get_batch(self) -> Tuple[np.ndarray, np.ndarray]:
+    def get_batch(self) -> Tuple[cp.ndarray, cp.ndarray]:
         # Return batch of shape (batch_size, seq_len)
         inputs = []
         targets = []
         for _ in range(self.batch_size):
-            start = np.random.randint(0, len(self.data) - self.seq_len - 1)
+            start = cp.random.randint(0, len(self.data) - self.seq_len - 1)
             x = self.data[start : start + self.seq_len]
             y = self.data[start + 1 : start + self.seq_len + 1]
             inputs.append(x)
             targets.append(y)
-        return np.array(inputs), np.array(targets)
+        return cp.array(inputs), cp.array(targets)
